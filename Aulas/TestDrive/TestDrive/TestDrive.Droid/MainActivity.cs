55e79df0575eed81428a11119767752dd6528caa
marcelooliveira <mclricardo@gmail.com>
2016-11-17 17:19:00 -0200

    TestDrive.Aula2 adicionado

--- Aulas/TestDrive/TestDrive/TestDrive.Droid/MainActivity.cs
+++ Aulas/TestDrive/TestDrive/TestDrive.Droid/MainActivity.cs
@@ -20,7 +20,7 @@ namespace TestDrive.Droid
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
-            LoadApplication(new App());
+            LoadApplication(new TestDrive.App());
         }
     }
 }
