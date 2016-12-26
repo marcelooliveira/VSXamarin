5072b27d693537917ed17f538b255c6c7181cb9e
marcelooliveira <mclricardo@gmail.com>
2016-12-26 17:04:32 -0200

    Aula 2

---
+++ TestDrive2/TestDrive2/TestDrive2.Aula2/ViewModels/LoginViewModel.cs
@@ -0,0 +1,92 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace TestDrive2.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        const string URL_GET_LOGIN = "https://aluracar.herokuapp.com/login";
+
+        private string usuario;
+        public string Usuario
+        {
+            get { return usuario; }
+            set
+            {
+                usuario = value;
+                OnPropertyChanged(nameof(Usuario));
+                ((Command)EntrarCommand).ChangeCanExecute();
+            }
+        }
+
+        private string senha;
+
+        public string Senha
+        {
+            get { return senha; }
+            set
+            {
+                senha = value;
+                OnPropertyChanged(nameof(Senha));
+                ((Command)EntrarCommand).ChangeCanExecute();
+            }
+        }
+
+        private bool aguarde;
+        public bool Aguarde
+        {
+            get { return aguarde; }
+            set
+            {
+                aguarde = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand EntrarCommand { get; private set; }
+
+        public LoginViewModel()
+        {
+            EntrarCommand = new Command(async () =>
+            {
+                await DoLogin(new Login(usuario, senha));
+            }, 
+            () =>
+            {
+                return !string.IsNullOrEmpty(usuario)
+                        && !string.IsNullOrEmpty(senha);
+            });
+        }
+
+        private async Task DoLogin(Login login)
+        {
+            Aguarde = true;
+            HttpClient cliente = new HttpClient();
+
+            try
+            {
+                var resultado = 
+                    await cliente.GetStringAsync(
+                        string.Format("{0}?email={1}&senha={2}", 
+                            URL_GET_LOGIN, 
+                            login.Usuario, 
+                            login.Senha)
+                        );
+                MessagingCenter.Send<Login>(login, "SucessoLogin");
+            }
+            catch (Exception exc)
+            {
+                MessagingCenter.Send<Exception>(exc, "FalhaLogin");
+            }
+            Aguarde = false;
+        }
+    }
+}
---
+++ TestDrive2/TestDrive2/TestDrive2.Aula2/Views/LoginView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestDrive.Views;
+using Xamarin.Forms;
+
+namespace TestDrive2.Views
+{
+    public partial class LoginView : ContentPage
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            MessagingCenter.Subscribe<Login>(this, "SucessoLogin", 
+                (msg) =>
+                {
+                    Navigation.PushAsync(new ListagemView());
+                });
+
+            MessagingCenter.Subscribe<Exception>(this, "FalhaLogin",
+                (msg) =>
+                {
+                    DisplayAlert("Login", "Erro ao validar usuário e senha!", "Ok");
+                });
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+
+            MessagingCenter.Unsubscribe<Login>(this, "SucessoLogin");
+            MessagingCenter.Unsubscribe<Login>(this, "FalhaLogin");
+        }
+    }
+}
