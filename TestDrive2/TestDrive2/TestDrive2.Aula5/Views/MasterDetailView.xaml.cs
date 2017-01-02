7df2dc43bcb4da6f1f8e46173474c48c64558a84
marcelooliveira <mclricardo@gmail.com>
2017-01-02 13:16:41 -0200

    aula 5 - início

---
+++ TestDrive2/TestDrive2/TestDrive2.Aula5/Views/MasterDetailView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace TestDrive.Views
+{
+    public partial class MasterDetailView : MasterDetailPage
+    {
+        public MasterDetailView(Usuario usuario)
+        {
+            InitializeComponent();
+
+            this.Master = new MasterView(usuario);
+        }
+    }
+}
