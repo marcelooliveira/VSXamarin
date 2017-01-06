0a912e30bd64a83552d17fae150cca1de0fa2a6f
marcelooliveira <mclricardo@gmail.com>
2017-01-06 17:45:25 -0200

    Roteiro - Aula 2 - início

---
+++ Aulas/TestDrive2/TestDrive2/TestDrive2.Aula2/Login.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDrive
+{
+    public class Login
+    {
+        public string Usuario { get; private set; }
+        public string Senha { get; private set; }
+
+        public Login(string usuario, string senha)
+        {
+            if (string.IsNullOrEmpty(usuario))
+                throw new ArgumentException(nameof(usuario));
+
+            if (string.IsNullOrEmpty(senha))
+                throw new ArgumentException(nameof(senha));
+
+            this.Usuario = usuario;
+            this.Senha = senha;
+        }
+    }
+}
